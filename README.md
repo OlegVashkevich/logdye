1542fc2a7584a3a5bdf03b7c06ad8de156872354
Oleg Vashkevich <ugluk@tut.by>
2025-03-09 00:07:00 +0300

    Logdye

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # logdye
 Colored Line Formatter for Monolog
+## Features
+- monolog v3+
+- lightweight
+- 100% test coverage
+- phpstan max lvl
+- phpstan full strict rules
+
+## Install
+```shell
+composer requier olegv/logdye
+```
+
+```php
+<?php
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+use OlegV\Logdye;
+```
+## Usage
+```php
+//logger
+$logger = new Logger('Name');
+
+$formatter = new Logdye(
+    "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
+    "Y-m-d H:i:s"
+);
+
+$handler = new StreamHandler("php://stdout", Level::Debug);
+$handler->setFormatter($formatter);
+$logger->pushHandler($handler);
+
+$logger->Debug('');
+$logger->Debug('test');
+$logger->Info('test');
+$logger->Notice('test');
+$logger->Warning('test');
+$logger->Error('test');
+$logger->Critical('test');
+$logger->Alert('test');
+$logger->Emergency('test');
+```
+You will see:
+
+![Logs of ascending levels with different colors wrapping the level, spelling "Test"](images/default.png)
---
+++ tests/LogdyeTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+use OlegV\Logdye;
+use PHPUnit\Framework\TestCase;
+use Tests\Helper\Intercept;
+
+class LogdyeTest extends TestCase
+{
+    public function testMonologColored(): void
+    {
+        $states = [
+            'Debug' => '[0;37mDEBUG[0m',
+            'Info' => '[1;34mINFO[0m',
+            'Notice' => '[1;32mNOTICE[0m',
+            'Warning' => '[0;33mWARNING[0m',
+            'Error' => '[1;33mERROR[0m',
+            'Critical' => '[0;31;51mCRITICAL[0m',
+            'Alert' => '[1;30;41mALERT[0m',
+            'Emergency' => '[1;5;21;41mEMERGENCY[0m',
+        ];
+        stream_filter_register("intercept", Intercept::class);
+        // Create a logger instance
+        $logger = new Logger('test');
+
+        $formatter = new Logdye(
+            "%level_name%",
+            "",
+        );
+
+        // Add a handler to output logs to the standard output stream
+        $handler = new StreamHandler("php://stdout", Level::Debug);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        $logger->Debug("just activate");
+        //start stream
+        $stderr = $handler->getStream();
+        if (is_resource($stderr)) {
+            stream_filter_append($stderr, "intercept");
+        }
+
+        $logger->Debug('test');
+        $logger->Info('test');
+        $logger->Notice('test');
+        $logger->Warning('test');
+        $logger->Error('test');
+        $logger->Critical('test');
+        $logger->Alert('test');
+        $logger->Emergency('test');
+        //end stream
+        $this->assertSame(Intercept::$cache, implode('', $states));
+    }
+
+    public function testFulltextWithStateColored(): void
+    {
+        $logger = new Logger('Name');
+
+        $formatter = new Logdye(
+            "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
+            "Y-m-d H:i:s",
+            true,
+        );
+        $data = [1 => "11", '2' => '22'];
+        $handler = new StreamHandler("php://stdout", Level::Debug);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        $logger->Debug('');
+        $logger->Debug('test', [$data]);
+        $logger->Info('test');
+        $logger->Notice('test');
+        $logger->Warning('test');
+        $logger->Error('test');
+        $logger->Critical('test');
+        $logger->Alert('test');
+        $logger->Emergency('test');
+
+        $this->expectNotToPerformAssertions();
+    }
+}
